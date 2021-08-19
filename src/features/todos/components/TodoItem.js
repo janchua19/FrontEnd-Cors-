89d9fe3030abe27eb058e338984af555b0fbdfda
janchua19 <jan.edward.chua@oocl.com>
2021-08-19 21:46:11 +0800

    [FrontEnd-cors][Jan] add edit button

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -3,10 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteFromList, updateTodo } from '../../apis/todos';
 import {selectTodoById, ToggleTodo, DeleteTodo } from "../reducers/todosSlice";
 import "../styles/TodoItem.css";
+import { useState } from 'react';
+import { Modal} from 'antd';
 
 function TodoItem(props) {
     const todo = useSelector (state => selectTodoById(state, props.itemId));
     const dispatch = useDispatch();
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
     function handleClick() {
         updateTodo(props.itemId, {done: !todo.done}).then((response) => {
@@ -21,13 +24,30 @@ function TodoItem(props) {
         });
     }
 
-    const todoStatus = todo.done ? "done" : ""; // if done will name done if not will be blank
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+    
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
+    
+    
+    const todoStatus = todo.done ? "done" : ""; // if done will name done if not will be blank
 
     return (<div className = "TodoItem-Table">
             <span className= {`TodoItem-todo ${todoStatus}`} onClick = {handleClick}>{todo.text}</span>
-            <button className= "Edit" onClick = {onClickDelete}>Edit</button>
+            <button className= {`Edit ${todoStatus}`} onClick = {showModal}>Edit</button>
             <button className= "Delete" onClick = {onClickDelete}>X</button>
+
+            <Modal title="Edit Todo" type="primary" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <textarea className="text-area">{todo.text}</textarea>
+            </Modal>
     </div>);
 }
 
